e21c6db4124de6f869d7eb557d1bcecd811a6212
Patrick Kimber <patrick@kbsoftware.co.uk>
2022-01-14 09:07:17 +0000

    'ember-try.js' - update from recent add-on

--- config/ember-try.js
+++ config/ember-try.js
@@ -1,68 +1,78 @@
-'use strict';
+"use strict"
 
-const getChannelURL = require('ember-source-channel-url');
+const getChannelURL = require("ember-source-channel-url")
 
-module.exports = function() {
-  return Promise.all([
-    getChannelURL('release'),
-    getChannelURL('beta'),
-    getChannelURL('canary')
-  ]).then((urls) => {
-    return {
-      useYarn: true,
-      scenarios: [
-        {
-          name: 'ember-lts-2.12',
-          npm: {
-            dependencies: {
-              'ember-data': '^2.12.0'
-            },
-            devDependencies: {
-              'ember-source': '^2.12.0'
-            }
+module.exports = async function () {
+  return {
+    scenarios: [
+      {
+        name: "ember-lts-3.16",
+        npm: {
+          devDependencies: {
+            "ember-source": "~3.16.0"
           }
-        },
-        {
-          name: 'ember-lts-2.16',
-          npm: {
-            dependencies: {
-              'ember-data': '^2.16.0'
-            },
-            devDependencies: {
-              'ember-source': '^2.16.0'
-            }
-          }
-        },
-        {
-          name: 'ember-release',
-          npm: {
-            devDependencies: {
-              'ember-data': 'latest',
-              'ember-source': urls[0]
-            }
-          }
-        },
-        {
-          name: 'ember-beta',
-          npm: {
-            devDependencies: {
-              'ember-data': 'beta',
-              'ember-source': urls[1]
-            }
-          },
-          allowedToFail: true
-        },
-        {
-          name: 'ember-canary',
-          npm: {
-            devDependencies: {
-              'ember-data': 'canary',
-              'ember-source': urls[2]
-            }
-          },
-          allowedToFail: true
         }
-      ]
-    }
-  });
-};
+      },
+      {
+        name: "ember-lts-3.20",
+        npm: {
+          devDependencies: {
+            "ember-source": "~3.20.5"
+          }
+        }
+      },
+      {
+        name: "ember-release",
+        npm: {
+          devDependencies: {
+            "ember-source": await getChannelURL("release")
+          }
+        }
+      },
+      {
+        name: "ember-beta",
+        npm: {
+          devDependencies: {
+            "ember-source": await getChannelURL("beta")
+          }
+        }
+      },
+      {
+        name: "ember-canary",
+        npm: {
+          devDependencies: {
+            "ember-source": await getChannelURL("canary")
+          }
+        }
+      },
+      {
+        name: "ember-default-with-jquery",
+        env: {
+          EMBER_OPTIONAL_FEATURES: JSON.stringify({
+            "jquery-integration": true
+          })
+        },
+        npm: {
+          devDependencies: {
+            "@ember/jquery": "^1.1.0"
+          }
+        }
+      },
+      {
+        name: "ember-classic",
+        env: {
+          EMBER_OPTIONAL_FEATURES: JSON.stringify({
+            "application-template-wrapper": true,
+            "default-async-observers": false,
+            "template-only-glimmer-components": false
+          })
+        },
+        npm: {
+          ember: {
+            edition: "classic"
+          }
+        }
+      }
+    ]
+  }
+}
